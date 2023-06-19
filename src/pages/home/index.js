d548b5bd211168039e4f5addf9fd6a6668eeb94c
tuandat1811 <dat.hdt181100@gmail.com>
2023-06-19 23:24:19 +0700

    Adding hook to finalize login and register flow

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,37 +1,55 @@
-// ** MUI Imports
+import React from 'react'
 import Card from '@mui/material/Card'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
+import Button from '@mui/material/Button'
 
 const Home = () => {
+  const handleLogin = () => {
+    // Logic to navigate to the login.js file or the login page
+    window.location.href = '/login.js' // Replace '/login.js' with the actual path to login.js
+  }
+
+  const handleRegister = () => {
+    // Logic to navigate to the register.js file or the register page
+    window.location.href = '/register.js' // Replace '/register.js' with the actual path to register.js
+  }
+
   return (
-    <Grid container spacing={6}>
-      <Grid item xs={12}>
-        <Card>
-          <CardHeader title='Kick start your project 🚀'></CardHeader>
-          <CardContent>
-            <Typography sx={{ mb: 2 }}>All the best for your new project.</Typography>
-            <Typography>
-              Please make sure to read our Template Documentation to understand where to go from here and how to use our
-              template.
-            </Typography>
-          </CardContent>
-        </Card>
+    <>
+      <nav>
+        <Button onClick={handleLogin}>Login</Button>
+        <Button onClick={handleRegister}>Register</Button>
+      </nav>
+      <Grid container spacing={6}>
+        <Grid item xs={12}>
+          <Card>
+            <CardHeader title='Kick start your project 🚀'></CardHeader>
+            <CardContent>
+              <Typography sx={{ mb: 2 }}>All the best for your new project.</Typography>
+              <Typography>
+                Please make sure to read our Template Documentation to understand where to go from here and how to use
+                our template.
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <CardHeader title='ACL and JWT 🔒'></CardHeader>
+            <CardContent>
+              <Typography sx={{ mb: 2 }}>
+                Access Control (ACL) and Authentication (JWT) are the two main security features of our template and are
+                implemented in the starter-kit as well.
+              </Typography>
+              <Typography>Please read our Authentication and ACL Documentations to get more out of them.</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <Card>
-          <CardHeader title='ACL and JWT 🔒'></CardHeader>
-          <CardContent>
-            <Typography sx={{ mb: 2 }}>
-              Access Control (ACL) and Authentication (JWT) are the two main security features of our template and are implemented in the starter-kit as well.
-            </Typography>
-            <Typography>Please read our Authentication and ACL Documentations to get more out of them.</Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-    </Grid>
+    </>
   )
 }
 
